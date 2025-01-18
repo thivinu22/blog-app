93875ebca208f420c60dbff32b4586dda0282a05
thivinu22 <thivinu.22@cse.mrt.ac.lk>
2025-01-18 12:51:47 +0530

    post not working

--- app/admin/addProduct/page.jsx
+++ app/admin/addProduct/page.jsx
@@ -1,7 +1,9 @@
 "use client"
 import { assets } from '@/Assets/assets'
+import axios from 'axios'
 import Image from 'next/image'
 import React, { useState } from 'react'
+import { toast } from 'react-toastify'
 
 const page = () => {
 
@@ -20,12 +22,27 @@ const page = () => {
       const value = event.target.value;
       setData((data) => ({...data,[name] : value}))
 
-      console.log(data); 
+      // console.log(data); 
   }
 
   const onSubmitHandler = async (e) => {
 
         e.preventDefault();
+        const formData = new FormData();
+        formData.append('title',data.title);
+        formData.append('description',data.description);
+        formData.append('category',data.category);
+        formData.append('author',data.author);
+        formData.append('authorImg',data.authorImg);
+        formData.append('image',image);
+
+        const response = await axios.post('/api/blog',formData);
+
+        if(response.data.success){
+          toast.success(response.data.msg);
+        } else{
+          toast.error("Error");
+        }
 
   }
 
