bd0777cc522401c7a75f088fdfb6c057ff8a9887
thivinu22 <thivinu.22@cse.mrt.ac.lk>
2025-01-18 12:29:36 +0530

    onChange done

--- app/admin/addProduct/page.jsx
+++ app/admin/addProduct/page.jsx
@@ -1,9 +1,62 @@
 "use client"
-import React from 'react'
+import { assets } from '@/Assets/assets'
+import Image from 'next/image'
+import React, { useState } from 'react'
 
 const page = () => {
+
+  const [image,setImage] = useState(false);
+
+  const [data,setData] = useState({
+    title: "",
+    description: "",
+    category:"Startup",
+    author: "James Author",
+    authorImg: "/author_img.png"
+  })
+
+  const onChangeHandler = (event) => {
+      const name = event.target.name;
+      const value = event.target.value;
+      setData((data) => ({...data,[name] : value}))
+
+      console.log(data); 
+  }
+
+  const onSubmitHandler = async (e) => {
+
+        e.preventDefault();
+
+  }
+
   return (
-    <div></div>
+    <div>
+      <form onSubmit={onSubmitHandler} className='pt-5 px-5 sm:pt-12 sm:pl-16' action="">
+          <p className='text-xl'>Upload Thumbnail</p>
+          <label htmlFor="image">
+            <Image className='mt-4' src={!image? assets.upload_area : URL.createObjectURL(image)} width={140} height={70} alt=''/>
+          </label>
+          <input onChange={(e) => {setImage(e.target.files[0])}} type="file" id='image' hidden  required/>
+          
+          <p className='text-xl mt-4'>Blog Titile</p>
+          <input type="text" name='title' value={data.title} onChange={onChangeHandler} placeholder='Type here' required className='w-full sm:w-[500px] mt-4 px-4 py-3 border'/>
+          
+          <p className='text-xl mt-4'>Blog Description</p>
+          <textarea type="text" name='description' onChange={onChangeHandler} value={data.description} placeholder='Write content here' rows={6} required className='w-full sm:w-[500px] mt-4 px-4 py-3 border'/>
+          
+          <p className='text-xl mt-4'>Blog Category</p>
+          <select name="category" onChange={onChangeHandler} value={data.category} className='w-40 mt-4 px-4 py-3 border text-gray-500'>
+            <option value="Startup">Startup</option>
+            <option value="Technology">Technology</option>
+            <option value="Lifestyle">Lifestyle</option>
+          </select>
+          
+          
+          
+          <br />
+          <button type='submit' className='mt-8 w-40 h-12 bg-black text-white'>ADD</button>
+      </form>
+    </div>
   )
 }
 
