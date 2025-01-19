45f8ae430577dcd513757d92b82679db77542d21
thivinu22 <thivinu.22@cse.mrt.ac.lk>
2025-01-19 09:30:04 +0530

    admin blog list done

--- app/admin/blogList/page.jsx
+++ app/admin/blogList/page.jsx
@@ -1,9 +1,53 @@
 "use client"
-import React from 'react'
+import BlogTableItem from '@/components/AdminComponents/BlogTableItem'
+import axios from 'axios';
+import React, { useEffect, useState } from 'react'
 
 const page = () => {
+
+    const [blogs,setBlogs] = useState([]);
+
+    const fetchBlogs = async () => {
+
+      const response = await axios.get('/api/blog');
+      console.log(response.data.blogs);
+      setBlogs(response.data)
+    }
+
+    useEffect(() => {
+      fetchBlogs();
+
+    }, []);
+
   return (
-    <div></div>
+    <div className='flex-1 pt-5 px-5 sm:pt-12 sm:pl-16'>
+    <h1>All Blogs</h1>
+    <div className='relative h-[80vh] max-w-[850px] overflow-x-auto mt-4 brder-gray-400 scrollbar-hide '>
+
+    <table className='w-full text-sm text-gray-500'>
+      <thead className='text-sm text-gray-700 text-left uppercase bg-gray-50'>
+        <tr>
+          <th scope='col' className='hidden sm:block px-6 py-3'>Author name</th>
+          <th scope='col' className='px-6 py-3'>Blog title</th>
+          <th scope='col' className='px-6 py-3'>Date</th>
+          <th scope='col' className='px-6 py-3'>Action name</th>
+        </tr>
+      </thead>
+      <tbody>
+        {blogs.map((item,index) => {
+            
+            return <BlogTableItem key={index} mongoId={item._id} title={item.title} author={item.author} authorImg={item.authorImg} date={item.date} />
+          
+        })
+
+        }
+      </tbody>
+
+    </table>
+
+    </div> 
+
+    </div>
   )
 }
 
---
+++ components/AdminComponents/BlogTableItem.jsx
@@ -0,0 +1,32 @@
+import { assets } from '@/Assets/assets'
+import Image from 'next/image'
+import React from 'react'
+
+const BlogTableItem = ({authorImg,title,author,date}) => {
+
+    // const blogDate = new Date(date);
+    const dateString = "2025-01-18T08:57:55.045Z";
+    const parts = dateString.split("T")[0].split("-");
+    const dateObject = new Date(parts[0], parts[1] - 1, parts[2]); // Year, Month (0-indexed), Day
+    // console.log(dateObject.toDateString());
+
+  return (
+    <tr className='bg-white border-b'>
+        <th scope='row' className='items-center gap-3 hidden sm:flex px-6 py-4 font-medium text-gray-900 whitespace-nowrap'>
+            <Image width={40} height={40} src={authorImg? authorImg : assets.profile_icon} alt='' />
+            <p>{author ? author : "no author "}</p>
+        </th> 
+        <td className='px-6 py-4'>
+            {title? title : "no title"}
+        </td>
+        <td className='px-6 py-4'>
+            {dateObject.toDateString()}
+        </td>
+        <td className='px-6 py-4 cursor-pointer'>
+            x
+        </td>
+    </tr>
+  )
+}
+
+export default BlogTableItem
